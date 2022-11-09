b4450d82b60f4004a4c61a63958336055400a10d
Shweta Kanekar <kanekarshweta10@gmail.com>
2022-11-09 20:24:24 +0530

    Created time tracking dashboard app using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,131 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'time-tracking-dashboard-angular';
+export class AppComponent implements OnInit {
+  optionSelected: string = 'weekly';
+
+  data = [
+    {
+      title: 'Work',
+      timeframes: {
+        daily: {
+          current: 5,
+          previous: 7,
+        },
+        weekly: {
+          current: 32,
+          previous: 36,
+        },
+        monthly: {
+          current: 103,
+          previous: 128,
+        },
+      },
+    },
+    {
+      title: 'Play',
+      timeframes: {
+        daily: {
+          current: 1,
+          previous: 2,
+        },
+        weekly: {
+          current: 10,
+          previous: 8,
+        },
+        monthly: {
+          current: 23,
+          previous: 29,
+        },
+      },
+    },
+    {
+      title: 'Study',
+      timeframes: {
+        daily: {
+          current: 0,
+          previous: 1,
+        },
+        weekly: {
+          current: 4,
+          previous: 7,
+        },
+        monthly: {
+          current: 13,
+          previous: 19,
+        },
+      },
+    },
+    {
+      title: 'Exercise',
+      timeframes: {
+        daily: {
+          current: 1,
+          previous: 1,
+        },
+        weekly: {
+          current: 4,
+          previous: 5,
+        },
+        monthly: {
+          current: 11,
+          previous: 18,
+        },
+      },
+    },
+    {
+      title: 'Social',
+      timeframes: {
+        daily: {
+          current: 1,
+          previous: 3,
+        },
+        weekly: {
+          current: 5,
+          previous: 10,
+        },
+        monthly: {
+          current: 21,
+          previous: 23,
+        },
+      },
+    },
+    {
+      title: 'Self Care',
+      timeframes: {
+        daily: {
+          current: 0,
+          previous: 1,
+        },
+        weekly: {
+          current: 2,
+          previous: 2,
+        },
+        monthly: {
+          current: 7,
+          previous: 11,
+        },
+      },
+    },
+  ];
+
+  ngOnInit() {
+    this.optionSelected = 'weekly';
+    let optionElement = document.getElementById(this.optionSelected)!;
+    optionElement.classList.add('activeOption');
+  }
+
+  onOptionClick(optionName: string) {
+    this.optionSelected = optionName;
+    let options = document.getElementsByTagName('li');
+    for (let i = 0; i < options.length; i++) {
+      options[i].classList.remove('activeOption');
+    }
+    let optionElement = document.getElementById(optionName)!;
+    optionElement.classList.add('activeOption');
+  }
 }
